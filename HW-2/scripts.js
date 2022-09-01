04f47755b2ebf0b4c685dc145a3e62ff204b0672
curiousMaatt <matwybygai@gmail.com>
2022-09-01 13:23:05 +0300

    refactored/HW-2

--- HW-2/scripts.js
+++ HW-2/scripts.js
@@ -1,26 +1,33 @@
 
-let N;
-let M;
+let min;
+let max;
 
-while(isNaN(N)) {
-    N = Math.round(prompt("Enter the number from which we will start to sum: ", 10));
+while(isNaN(min)) {
+    min = Math.round(prompt("Enter the number from which we will start to sum: ", 10));
 }
 
-while(isNaN(M) ||  M <= N ) {
-    M = Math.round(prompt("Enter the number till which we will sum: (It has to be greater than N)", 12));
+while(isNaN(max) ||  max <= min ) {
+    max = Math.round(prompt("Enter the number till which we will sum: (It has to be greater than N)", 12));
 }
 
-let numbersType = confirm("Should we miss even numbers?")
+let skipEvenNumbers = confirm("Should we miss even numbers?")
 let sum = 0;
 
-for(let i = N; i <= M; ++i) {
-    if(numbersType) {
-        if( i % 2 != 0) {
-            sum += i;    
-        }
+function isOdd(num){
+    if(num % 2 != 0) {
+        return true;
     }
-    else
+    else false;
+}
+
+for(let i = min; i <= max; ++i) {    
+    if(skipEvenNumbers && isOdd(i)) {
         sum += i;
+    }
+    else if (skipEvenNumbers == false) {
+        sum += i;
+    }
+
 }
 
 console.log("The sum is equal to: ");
